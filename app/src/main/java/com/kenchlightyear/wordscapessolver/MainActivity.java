1259fe4ff33f9634a3c37af98f9cd6267383236b
Joebert Jacaba <joebertj@gmail.com>
2018-11-02 11:44:15 +0800

    added AppRate for veteran users

--- app/src/main/java/com/kenchlightyear/wordscapessolver/MainActivity.java
+++ app/src/main/java/com/kenchlightyear/wordscapessolver/MainActivity.java
@@ -18,10 +18,16 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import me.msfjarvis.apprate.AppRate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        new AppRate(this)
+                .setMinDaysUntilPrompt(7)
+                .setMinLaunchesUntilPrompt(20)
+                .init();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -97,18 +103,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static void hideKeyboard(Activity activity) {
-        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        //Find the currently focused view, so we can grab the correct window token from it.
-        View view = activity.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = new View(activity);
-        }
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
-
-    public static void hideKeyboardFrom(Context context, View view) {
+    private static void hideKeyboardFrom(Context context, View view) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
